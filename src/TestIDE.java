3113d6a620816ce56b8301cda89df6a59546b072
benjaminbylund <te16.benjamin.bylund@umea.nti.se>
2017-11-06 11:14:52 +0100

    Klar

--- src/TestIDE.java
+++ src/TestIDE.java
@@ -1,12 +1,30 @@
-/*
+/*  Math.random 1-100, användaren gissar på talet
     Benjamin Bylund
     2017-11-06
 */
 
-
+import java.util.Scanner;
 
 public class TestIDE {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+
+        int tal = (int)(Math.random()*101);
+
+
+        while (true) {
+            int svar = sc.nextInt();
+            if(tal == svar){
+                System.out.println("Rätt");
+                break;
+            }
+            else if (tal < svar) {
+                System.out.println("Fel talet är mindre");
+            }
+            else if (tal > svar) {
+                System.out.println("Fel talet är större");
+            }
+        }
     }
 }
